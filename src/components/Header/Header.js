3bceb1a46d6ffe748fee27c01bb8e86c848741c6
Alexander Belyakov <belyakov-alexander-2017@yandex.ru>
2024-03-14 18:50:03 +0300

    feat: header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,17 @@
 import './Header.css';
 import logo from '../../images/logo.png'
+import phone from '../../images/phone.png'
+import { Navigation } from '../Navigation/Navigation';
 
 export function Header() {
     return (
         <header className="header">
-            <img src={logo} alt="Логотип"/>
+            <img className="header__logo" src={logo} alt="Логотип"/>
+            <Navigation />
+            <address class="header__address">
+                <img class="header__address-phone" src={phone} alt="Телефон"/>
+                <a class="header__address-link" href="tel:+83451233445">8-345-123-34-45</a>
+            </address>
         </header>
     )
 }
